90d56cc2175649594a1f8f032b7f2d2cccd38366
jinnie <genie121110@gmail.com>
2024-06-13 14:05:14 +0900

    [feature] p.39

--- test_game.py
+++ test_game.py
@@ -19,8 +19,7 @@ class TestGame(TestCase):
         self.assert_illegal_argument(None)
         self.assert_illegal_argument("12")
         self.assert_illegal_argument("1234")
-
-
+        self.assert_illegal_argument("12s")
 
 
 
