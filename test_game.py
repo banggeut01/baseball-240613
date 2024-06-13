30050e042491aa091d667b904dcade8dcff44146
jinnie <genie121110@gmail.com>
2024-06-13 14:04:00 +0900

    [refactor] p.37

--- test_game.py
+++ test_game.py
@@ -7,13 +7,20 @@ class TestGame(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.game = Game()
-    def test_exception_when_input_is_none(self):
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
 
-    def test_exception_when_input_length_is_unmatched(self):
-        with self.assertRaises(TypeError):
-            self.game.guess("12")
+    def assert_illegal_argument(self, guessNumber):
+        try:
+            self.game.guess(guessNumber)
+            self.fail()
+        except TypeError:
+            pass
+
+    def test_exception_when_invalid_input(self):
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
+
+
 
 
 
